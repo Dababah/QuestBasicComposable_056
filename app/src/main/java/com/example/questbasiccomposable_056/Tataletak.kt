196aa081a8534993a7546d3e9ec475e847af4345
Dababah <pawasalee96@gamil.com>
2025-10-16 16:07:55 +0700

    menambah composable TataletakColumnRow

--- app/src/main/java/com/example/questbasiccomposable_056/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_056/Tataletak.kt
@@ -64,4 +64,20 @@ fun TataletakBox(modifier: Modifier) {
         Text(text = "Column 2")
     }
 }
+@Composable
+fun TataletakColumnRow(modifier: Modifier) {
+    Column() {
+
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
+        }
+
+
+
+        }
+
+
+
+
 
