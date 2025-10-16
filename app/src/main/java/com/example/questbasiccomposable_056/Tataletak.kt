6a928065e4d169200c55f4a318013080336fa940
Dababah <pawasalee96@gamil.com>
2025-10-16 16:13:57 +0700

    menambah tata letak Row dan Column dengan contoh komponen teks

--- app/src/main/java/com/example/questbasiccomposable_056/Tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_056/Tataletak.kt
@@ -97,6 +97,43 @@ fun TataletakColumnRow(modifier: Modifier) {
         }
     }
 
+    @Composable
+    fun TataLetakRowColumn(modifier: Modifier) {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            // Kolom 1
+            Column {
+                Text(text = "Komponen1Kolom1")
+                Text(text = "Komponen2Kolom1")
+                Text(text = "Komponen3Kolom1")
+            }
+
+            // Kolom 2
+            Column {
+                Text(text = "Komponen1Kolom2")
+                Text(text = "Komponen2Kolom2")
+                Text(text = "Komponen3Kolom2")
+            }
+        }
+    }
+
+    @Composable
+    fun TataLetakRowColumn2(modifier: Modifier) {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            // Kolom 2
+            Column {
+                Text(text = "Komponen1Kolom2")
+                Text(text = "Komponen2Kolom2")
+                Text(text = "Komponen3Kolom2")
+            }
+        }
+    }
+
 
 
 
